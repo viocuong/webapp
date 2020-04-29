97bc2d61b4e44f4b891b8701b3803e3db87e4b20
viocuong <viocuong@users.noreply.github.com>
2020-04-29 15:02:37 +0700

    update

--- mvc/controllers/admin.php
+++ mvc/controllers/admin.php
@@ -1,9 +1,7 @@
 <?php
     class admin extends Controller{
         function default(){
-            if(empty($_SESSION['user'])){
-                header('Location: login');
-            }
+            
             $md=$this->requireModel('AdminModel');
             $this->view('layoutadmin');
         }
