79c99d55d8f7657997571ee50fff77f3e01a3264
viocuong <viocuong@users.noreply.github.com>
2020-05-07 21:34:02 +0700

    update

--- mvc/models/adminModel.php
+++ mvc/models/adminModel.php
@@ -36,8 +36,8 @@
         public function getSearch($str){
             $id=$str;
             $str="%{$str}%";
-            if($stmt=$this->conn->prepare("SELECT tb_account.userName,tb_order.id_order,tb_order.content,tb_order.date,tb_order.price,tb_order.status_tranport,tb_order.status_pay,tb_order.link_fb from tb_order,tb_account where (tb_order.userName=tb_account.userName) and(tb_order.id_order=? or tb_order.userName like ? or tb_order.date like ? or tb_order.link_fb like ? or tb_order.content like ?)")){
-                $stmt->bind_param("issss",$id,$str,$str,$str,$str);
+            if($stmt=$this->conn->prepare("SELECT tb_account.userName,tb_order.id_order,tb_order.content,tb_order.date,tb_order.price,tb_order.status_tranport,tb_order.status_pay,tb_order.link_fb from tb_order,tb_account where (tb_order.userName=tb_account.userName) and(tb_order.id_order=? or tb_order.userName like ? )")){
+                $stmt->bind_param("is",$id,$str);
                 $stmt->execute();
                 $result=$stmt->get_result();
                 $stmt->fetch();
