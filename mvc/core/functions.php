a04a7c912b31e249fb0ceef9520f6b3b4012ef64
viocuong <viocuong@users.noreply.github.com>
2020-08-06 01:18:49 +0700

    up

--- mvc/core/functions.php
+++ mvc/core/functions.php
@@ -22,7 +22,7 @@ class Functions
         $mail = new PHPMailer(true);                              // Passing `true` enables exceptions
         try {
             //Server settings
-            $mail->SMTPDebug = 2;                                 // Enable verbose debug output
+            $mail->SMTPDebug = 0;                                 // Enable verbose debug output
             $mail->isSMTP();                                      // Set mailer to use SMTP
             $mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
             $mail->SMTPAuth = true;                               // Enable SMTP authentication
