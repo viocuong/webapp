6f3e6006bd192467e6cff1b781d919e77c0c06d2
viocuong <viocuong@users.noreply.github.com>
2020-08-09 17:54:37 +0700

    up

--- mvc/controllers/redirect.php
+++ mvc/controllers/redirect.php
@@ -5,7 +5,7 @@
                 $_SESSION['user']="admin";
                 header("Location:http://{$GLOBALS['HOST']}/webapp/admin/default/unsent");
             }
-            else if($id="9482e0b822adcb6341266ca852960e0c3a953294"){
+            else if($id=="9482e0b822adcb6341266ca852960e0c3a953294"){
                 //Cuong
                 $_SESSION['user']="nguyencuong";
                 header("Location:http://{$GLOBALS['HOST']}/webapp/client/default/createorder");
